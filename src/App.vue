c4c41f0583a3b3b5b856d5cbf255b9f8dbab038c
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-08-21 21:37:31 +0900

    fc

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script setup></script>
+
+<template>
+  <main class="main">
+    <div class="wrapper">
+      <div class="mv">
+        <div class="mv__inner">
+          <div class="mv__bg">
+            <div class="mv__item">
+              <div class="mv__logo">
+                <img src="" alt="" />
+              </div>
+              <h1 class="mv__title">モダン<br />ライフスタイル<br />ショップ</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </main>
+</template>
+
+<style lang="scss" scoped></style>
