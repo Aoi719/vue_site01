55dd7b8bc1bcdd25d2f098bfe5958b85ac48da8f
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-08-21 22:28:23 +0900

    メインビジュアル

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,11 @@
-<script setup></script>
+<script setup>
+import MV from './components/MainVisual.vue'
+</script>
 
 <template>
   <main class="main">
     <div class="wrapper">
-      <div class="mv">
-        <div class="mv__inner">
-          <div class="mv__bg">
-            <div class="mv__item">
-              <div class="mv__logo">
-                <img src="" alt="" />
-              </div>
-              <h1 class="mv__title">モダン<br />ライフスタイル<br />ショップ</h1>
-            </div>
-          </div>
-        </div>
-      </div>
+      <MV />
     </div>
   </main>
 </template>
---
+++ src/components/MainVisual.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="mv">
+    <div class="mv__bg">
+      <div class="mv__inner">
+        <div class="mv__item">
+          <div class="mv__logo">
+            <img src="/images/logo.svg" alt="" />
+          </div>
+          <h1 class="mv__title">モダン<br />ライフスタイル<br />ショップ</h1>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.mv {
+  min-height: 100vh;
+}
+.mv__bg {
+  min-height: inherit;
+  background: url(/images/mv.jpg) no-repeat center / cover;
+  position: relative;
+  &::after {
+    content: '';
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    position: absolute;
+    background-color: rgba($color: $black, $alpha: 0.5);
+    width: 100%;
+    height: 100%;
+    z-index: 2;
+  }
+}
+.mv__inner {
+  min-height: inherit;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.mv__item {
+  min-height: inherit;
+  position: relative;
+  z-index: 3;
+  display: flex;
+  align-items: center;
+}
+.mv__logo {
+  width: 150px;
+}
+.mv__title {
+  color: $white;
+  margin-left: 8px;
+  font-size: fz(48);
+  line-height: 1.3;
+  @include mq() {
+    font-size: fz(40);
+  }
+}
+</style>
