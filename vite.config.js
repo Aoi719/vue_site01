cbd574d6ce9f75ccd8965fd11a16ea3df6c9ffb0
Aoi719 <n1sh1_works@yahoo.co.jp>
2025-08-21 22:28:14 +0900

    scss設定

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,22 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    vue(),
-    vueDevTools(),
-  ],
+  plugins: [vue(), vueDevTools()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        // 全コンポーネントで変数・mixinを自動読み込み
+        additionalData: `
+          @use "@/assets/scss/settings/variables" as *;
+          @use "@/assets/scss/settings/mixins" as *;
+          @use "@/assets/scss/settings/functions" as *;
+        `,
+      },
     },
   },
 })
